ba08925b826a70c6513ae48b2a48db6de65d8295
weiyuhang <2089853250@qq.com>
2019-01-27 21:22:00 +0800

    fghh

--- src/main/java/com/cdtu/controller/dfghj.java
+++ src/main/java/com/cdtu/controller/dfghj.java
@@ -4,6 +4,6 @@ import org.apache.log4j.chainsaw.Main;
 
 public class dfghj {
 public static void main(String[] args) {
-	System.out.println("cdzvcz");
+	System.out.println("cdhcgvbnm,jhm,zvcz");
 }
 }
