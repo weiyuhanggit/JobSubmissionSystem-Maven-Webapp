5e21bacc2573ed7e79f91d9932f25c9679ef3470
weiyuhang <2089853250@qq.com>
2019-01-27 21:18:08 +0800

    cnngnmvmv

--- src/main/java/com/cdtu/controller/dfghj.java
+++ src/main/java/com/cdtu/controller/dfghj.java
@@ -5,5 +5,6 @@ import org.apache.log4j.chainsaw.Main;
 public class dfghj {
 public static void main(String[] args) {
 	System.out.println("cdzv54wdsfdw45w5cz");
+	System.out.println("fewgfgr");
 }
 }
